62b3eb814f5369f29f424a9c8a1216c72cbd7ef0
quol <kuolamol77@gmail.com>
2025-03-25 12:07:22 +0300

    adding some stylying to the faqs section

--- project2/home.js
+++ project2/home.js
@@ -1,14 +1,2 @@
 
 const questions= document.querySelectorAll('.questions');
-const answers= document.querySelectorAll('.questions p');
-
-questions.addEventListener('click',()=> {
-    if(answers.style.display==='none'){
-        answers.style.display='block';
-        answers.style.color='red';
-        answers.style.fontWeight='bold';
-    }else{
-        answers.style.display='none';
-    }
-   
-});
