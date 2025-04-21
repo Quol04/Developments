e74e301d67361cab33a32eeb29e8481a5448a2f6
quol <kuolamol77@gmail.com>
2025-04-21 20:37:54 +0300

    added some js scripts

---
+++ DOM/modal/modal.js
@@ -0,0 +1,4 @@
+
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
